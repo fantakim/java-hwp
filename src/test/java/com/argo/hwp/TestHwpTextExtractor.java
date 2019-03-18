f146098f93d642c6de4a26cc2f52e2346c33baf7
fanta <chunjoo0@naver.com>
2019-03-18 22:40:46 +0900

    add tests for HwpTextExtractor

---
+++ src/test/java/com/argo/hwp/TestHwpTextExtractor.java
@@ -0,0 +1,33 @@
+package com.argo.hwp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TestHwpTextExtractor {
+	@Test
+	public void testExtract() throws FileNotFoundException, IOException {
+		ArrayList<File> files = new ArrayList<File>();
+		files.add(getResourceFile("v3/4e00-62ff.hwp"));
+		files.add(getResourceFile("v3/han_special_char_3.0.hwp"));
+		files.add(getResourceFile("v5/han_grammar.hwp"));
+		files.add(getResourceFile("v5/han_special_char.hwp"));
+		
+		for(File file : files){
+			Writer writer = new StringWriter();
+			HwpTextExtractor.extract(file, writer);
+			
+			String text = writer.toString();
+			System.out.println(text);
+		}
+	}
+	
+	private File getResourceFile(String path) {
+		return new File(getClass().getResource("/" + path).getFile());
+	}
+}
